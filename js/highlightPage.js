378fe183e7800b10d2cf52deec2fa462724e86b4
thinh20cm <nguyenducthinh05072000@gmail.com>
2020-04-14 20:43:27 +0300

    html deleted

--- js/highlightPage.js
+++
@@ -1,7 +0,0 @@
-let curPage = document.URL;
-console.log(curPage);
-document.querySelectorAll(".nav-link").forEach(function (e) {
-    if (e.href == curPage) {
-        e.classList.add("current");
-    }
-});
