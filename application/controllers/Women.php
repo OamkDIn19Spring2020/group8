51f78538da6255560666a79049aa75976cd4e7aa
quyentrankhanhha <t9qutr00@students.oamk.fi>
2020-04-21 15:54:41 +0300

    april21-2

---
+++ application/controllers/Women.php
@@ -0,0 +1,17 @@
+<?php
+class Women extends CI_Controller
+{
+    public function index()
+    {
+        $data = array(
+            'title' => 'Women',
+            'home' =>  '<a class="nav-item nav-link" style="color:white" href="' . base_url() . 'home">HOME</a>',
+            'men' => '<a class="nav-item nav-link" href="' . base_url() . '">MEN</a>',
+            'women' => '<a class="nav-item nav-link" href="' . base_url() . '">WOMEN</a>',
+            'kids' => '<a class="nav-item nav-link" href="' . base_url() . '">KIDS</a>',
+            'news' => '<a class="nav-item nav-link" href="' . base_url() . '">NEWS</a>',
+            'contact' => '<a class="nav-item nav-link" href="' . base_url() . 'contact">CONTACT</a>'
+        );
+        $this->load->view('women/women', $data);
+    }
+}
